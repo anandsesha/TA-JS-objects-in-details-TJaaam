e74d4bd2418d797ba70a7656eaef3a6e240406e9
Anand Seshadri <anandseshawork@gmail.com>
2021-11-22 23:07:08 +0530

    refactor book app

--- block-BJaail/code/index.js
+++ block-BJaail/code/index.js
@@ -1,3 +1,11 @@
+let form = document.querySelector('form')
+let bookListRoot = document.querySelector('.book_list')
+
+
+const titleElm = form.elements.bookTitle;
+const authorElm = form.elements.bookAuthor;
+const isbnElm = form.elements.bookIsbn;
+
 // BookList will manage each individual Book
 class BookList{
     constructor(root, list = []){
@@ -8,27 +16,50 @@ class BookList{
         let book = new Book(title,author,isbn);
         this.books.push(book)
         this.createUI()
-        return this.books.length;
+        // return this.books.length;
     }
-    chnageState(){
-
-    }
-    handleDelete(id){
-        let index = this.books.findIndex(book => book.id === id)
-        this.books.splice(index,1)
-        this.createUI();
-        return this.books.length;
-    }
+    // handleDelete(id){
+    //     let index = this.books.findIndex(book => book.id === id)
+    //     this.books.splice(index,1)
+    //     this.createUI();
+    //     return this.books.length;
+    // }
+    // createDeleteUI(){
+    //     this.root.innerHTML = "";
+    //     this.books.forEach((book) => {
+    //         let ui = book.createUI()
+    //         ui.querySelector('button').addEventListener(
+    //             'click',this.handleDelete.bind(this,book.id)
+    //         );
+    //         this.root.append(ui);
+    //     });
+    // }
     createUI(){
-        this.root.innerHTML = "";
         this.books.forEach((book) => {
-            let ui = book.createUI()
-            ui.querySelector('button').addEventListener(
-                'click',this.handleDelete.bind(this,book.id)
-            );
-            this.root.append(ui);
-        });
-    }
+            bookListRoot.innerHTML = "";
+            let li = document.createElement('li')
+            li.classList.add('flex')
+
+            let title = document.createElement('h5')
+            title.innerText = book.title;
+            let author = document.createElement('h5')
+            author.innerText = book.author;
+            let isbn = document.createElement('h5')
+            isbn.innerText = book.isbn;
+            
+            let cross = document.createElement('button')
+            cross.classList.add('noStyle')
+            cross.innerText = book.isRead ? `✅` : `❌`;
+            cross.addEventListener('click',() => {
+                book.toggleIsRead();
+                this.createUI();
+            })
+
+            li.append(title,author,isbn,cross)
+            bookListRoot.append(li)
+            // return li;
+        })
+    }   
 }
 
 // For Each Book
@@ -38,39 +69,35 @@ class Book{
         this.author = author;
         this.isbn = isbn;
         this.id = `id-${Date.now()}`
+        this.isRead = false;
     }
-    createUI(){
-        // let ul = document.querySelector('.book_list')
-        let li = document.createElement('li')
-        li.classList.add('flex')
-
-        let title = document.createElement('h5')
-        title.innerText = this.title;
-        let author = document.createElement('h5')
-        author.innerText = this.author;
-        let isbn = document.createElement('h5')
-        isbn.innerText = this.isbn;
-        
-        let cross = document.createElement('button')
-        cross.innerText = `❌`;
-        // cross.addEventListener('click',handleDelete)
-
-        li.append(title,author,isbn,cross)
-        return li;
+    toggleIsRead(){
+        this.isRead = !this.isRead;
     }
 }
 
-let addBookButton = document.querySelector('.addBook')
 
-let myBook = new BookList(document.querySelector('.book_list'))
-myBook.addBook("Alchemist","Khalid Hosneni",20)
-myBook.addBook("A Thousand Suns","Khalid Hosneni",21)
+let library = new BookList() 
 
-// addBookButton.addEventListener('keyup',(event) => {
-//     if(event.keycode === 13){
-//         myBook.addBook(event.target.value)
-//     }
-// })
+function handleSubmit(event){
+    event.preventDefault();
+    const title = titleElm.value;
+    const author = authorElm.value;
+    const isbn = isbnElm.value;
+    console.log(title,author,isbn)
+    library.addBook(title,author,isbn)
+    titleElm.value = '';
+    authorElm.value = '';
+    isbnElm.value = '';
+}
+
+form.addEventListener('submit',handleSubmit)
 
 
-// let firstBook = new Book("Harry Potter","J.K Rowling",10)
+
+// let addBookButton = document.querySelector('.addBook')
+
+// let myBook = new BookList(document.querySelector('.book_list'))
+// myBook.addBook("Alchemist","Khalid Hosneni",20)
+// myBook.addBook("A Thousand Suns","Khalid Hosneni",21)
+
